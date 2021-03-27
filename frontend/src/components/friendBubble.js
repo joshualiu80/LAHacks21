d3046457ddf14570306c5f2a4ac986c58cbc77fa
Joshua Liu <joshua.benjamin.liu@gmail.com>
2021-03-27 12:30:26 -0700

    Display friends via a friendslist and added user prop to Friend

--- frontend/src/components/friendBubble.js
+++ frontend/src/components/friendBubble.js
@@ -2,10 +2,11 @@ import React from 'react';
 import CurvedName from './curvedName';
 import './friendBubble.css';
 
-const FriendBubble = ({ username, profilePic, setShowPopUp }) => {
+const FriendBubble = ({ username, profilePic, setShowPopUp, setFriendToOpen }) => {
 
     const openFriend = () => {
         setShowPopUp(true);
+        setFriendToOpen(username);
     };
 
     return (
@@ -14,7 +15,8 @@ const FriendBubble = ({ username, profilePic, setShowPopUp }) => {
                 <img src={profilePic} alt={"no img found"} onClick={openFriend}/> 
                 {/* replace "no img found" with one of those anon profile pics */}
             </div>
-            <CurvedName text={username} arc={120} radius={30} className="name"/>
+            <p className="name">{username}</p>
+            {/* <CurvedName text={username} arc={120} radius={30} className="name"/> */}
             {/* sigh this dont work */}
         </div>
     );
--- frontend/src/pages/friendPage.js
+++ frontend/src/pages/friendPage.js
@@ -1,15 +1,32 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState, useMemo } from 'react';
+import axios from 'axios';
 import FriendBubble from '../components/friendBubble';
 import Friend from '../components/Friend';
 import './friendPage.css';
 
+const exampleIMG = "https://images.ctfassets.net/hrltx12pl8hq/6YSoTmOYPk2VtQ7JSkPuzS/8250a3d54c1a714aa5e57f6a2826509e/shutterstock_1554086789.jpg?fit=fill&w=480&h=270";
+
 const FriendPage = () => {
     const [showPopUp, setShowPopUp] = useState(false);
+    const [friendToOpen, setFriendToOpen] = useState("");
+    const [friendsList, setFriendsList] = useState([{username: 'Mingjia', profilePic: exampleIMG}, {username: 'Josh', profilePic: exampleIMG}]); //temp for testing
+
+    // useEffect(() => {
+    //     const res = axios.get('this is where the route goes, probably include userId from cookies');
+    //     setFriendsList(res);
+    // }
+    // , []);
+
+    const displayFriends = useMemo(() => friendsList.map(
+        (friend) => (
+            <FriendBubble username={friend.username} profilePic={friend.profilePic} setShowPopUp={setShowPopUp} setFriendToOpen={setFriendToOpen} />
+        )
+    ), [friendsList]);
 
     return (
         <div>
-            <FriendBubble username={"Mingjia"} profilePic={"https://images.ctfassets.net/hrltx12pl8hq/6YSoTmOYPk2VtQ7JSkPuzS/8250a3d54c1a714aa5e57f6a2826509e/shutterstock_1554086789.jpg?fit=fill&w=480&h=270"} setShowPopUp={setShowPopUp}/>
-            {showPopUp ? <Friend /> : null}
+            {displayFriends}
+            {showPopUp ? <Friend user={friendToOpen} /> : null}
         </div>
     );
 }
