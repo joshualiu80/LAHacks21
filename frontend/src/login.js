7d2aaf2e95910053985d20eb93845a36b3707797
Joshua Liu <joshua.benjamin.liu@gmail.com>
2021-03-27 00:05:13 -0700

    Login Updates

--- frontend/src/login.js
+++ frontend/src/login.js
@@ -1,20 +1,27 @@
 import React, { useState } from 'react';
 
 const Login = () => {
-    const [firstname, setFirstname] = useState("");
-    const [lastname, setLastname] = useState("");
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
+
+    const signUp = () => {
+        return (
+            <div>:)</div>
+        );
+    }
+
+    const submitLogin = (e) => {
+        e.preventDefault();
+    }
 
     return (
         <div className="Login">
             <img src="/" alt={"Logo Goes Here"}/>
-            <form className="loginForm" onSubmit={(e) => e.preventDefault()}>
-                <input className="firstname" type="text" placeholder="First Name" name="firstname" value={firstname} onChange={(e) => setFirstname(e.target.value)} />
-                <input className="lastname" type="text" placeholder="Last Name" name="lastname" value={lastname} onChange={(e) => setLastname(e.target.value)} />
+            <form className="loginForm" onSubmit={submitLogin}>
                 <input className="username" type="text" placeholder="Username" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
                 <input className="password" type="password" placeholder="Password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
                 <button type="submit">Login</button>
+                <p>Don't have an account? <span onClick={signUp}>Sign up</span> </p>
             </form>
         </div>
     );
