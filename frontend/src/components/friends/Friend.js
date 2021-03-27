da7728f26a50169431ecf48b00b10fe99425e3a4
Xuan Vu <xuanbee@yahoo.com>
2021-03-27 16:21:59 -0700

    schedule recording

--- frontend/src/components/friends/Friend.js
+++ frontend/src/components/friends/Friend.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect } from 'react'
+import React from 'react'
 import './Friend.css'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faComment, faClock, faEdit } from '@fortawesome/free-regular-svg-icons'
-import DateTimePicker from 'react-datetime-picker'
+import Schedule from './schedule.js'
 
 const DUMMYDATA = {
   fname: 'Xuan',
@@ -10,17 +10,6 @@ const DUMMYDATA = {
 }
 
 const Friend = ({ setShowPopUp }) => {
-  const [subject, setSubject] = useState('');
-  const [datetime, setDatetime] = useState(new Date());
-
-  useEffect(() => {
-    // const content = document.getElementsByClassName('tabcontent');
-    // for (let i = 0; i < content.length; i++) {
-    //   content[i].style.display = 'none';
-    // }
-    // document.getElementById('messages').style.display = 'block';
-  }, [])
-
   const openTab = (e, tab) => {
     const content = document.getElementsByClassName('tabcontent');
     const tabs = document.getElementsByClassName('tablinks');
@@ -46,42 +35,28 @@ const Friend = ({ setShowPopUp }) => {
   
   return (
     <>
-      <div class="overlay" onClick={() => {setShowPopUp(false)}}/>
-      <div class="friend">
-        <div class="tabs">
-          <button class="tablinks messages active-tab" onClick={(e) => {openTab(e, 'messages')}}>
+      <div className="overlay"/>
+      <div className="friend">
+        <div className="tabs">
+          <button className="tablinks messages active-tab" onClick={(e) => {openTab(e, 'messages')}}>
             <FontAwesomeIcon icon={faComment}/>
           </button>
-          <button class="tablinks schedule" onClick={(e) => {openTab(e, 'schedule')}}>
+          <button className="tablinks schedule" onClick={(e) => {openTab(e, 'schedule')}}>
             <FontAwesomeIcon icon={faClock} />
           </button>
-          <button class="tablinks settings" onClick={(e) => {openTab(e, 'settings')}}>
+          <button className="tablinks settings" onClick={(e) => {openTab(e, 'settings')}}>
             <FontAwesomeIcon icon={faEdit} />
           </button>
         </div>
-        <div id="messages" class="tabcontent">
+        <div id="messages" className="tabcontent">
           {friendInfo}
           <p>wait don't we need to store a user's messages too? bc snippets are just scheduled ones right?</p>
         </div>
-        <div id="schedule" class="tabcontent hidden">
+        <div id="schedule" className="tabcontent hidden">
           {friendInfo}
-          <form class="record-msg">
-            <input type="text" placeholder="Subject" name="subject" value={subject} onChange={(e) => setSubject(e.target.value)} />
-            <DateTimePicker
-              className="datetime"
-              onChange={setDatetime}
-              value={datetime}
-              disableClock
-              clearIcon={null}
-              minDate={new Date()}
-            />
-            <button className="record-btn">
-              click the moon to record
-              <img src="/images/moon.png"/>
-            </button>
-          </form>
+          <Schedule />
         </div>
-        <div id="settings" class="tabcontent hidden">
+        <div id="settings" className="tabcontent hidden">
           {friendInfo}
           <h3>Settings</h3>
           <p>unadd friend or something here</p>
