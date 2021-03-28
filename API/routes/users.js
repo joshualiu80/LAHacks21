ec32f657fd1a6d88fd4ecd00059bc040896432e2
nguyenethan01 <nguyenethan01@gmail.com>
2021-03-27 17:22:49 -0700

    finish branch

--- API/routes/users.js
+++ API/routes/users.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const mongoose = require('mongoose');
+const { db } = require('../models/user.model');
 const User = require('../models/user.model');
 
 router.post('/', function(req, res){
@@ -16,11 +17,45 @@ router.post('/', function(req, res){
   .catch(err => res.status(400).json("Error: " + err))
 });
 
-
+//get user by id
 router.get('/:id', function(req,res){
   User.findOne({_id:req.params.id})
   .then(user=>res.json(user))
   .catch(err=> res.status(400).json('Error: ' + err))
 });
+
+//add friend
+router.post('/addFriend', function(req,res){
+  User.findOneAndUpdate(
+    {_id:req.body.currId},
+    {$push:{"friends": req.body.friendId}},
+    function(error, success){
+      if (error){
+        console.log(error);
+      }else{
+        console.log(success);
+      }
+    }
+  )
+  .then(res.status(200).json('Friend Added'))
+  .catch(res.status(400).json('Error: ' + err))
+});
+
+router.get('/getFriends/:id', function(req,res){
+  User.findOne({_id:req.params.id})
+  .populate("friends").exec(
+  function(error, user){
+    if (error){
+      res.status(400).json('Error: ' + err);
+    }
+    else if (user){
+      res.status(200).send(user.friends);
+    }
+    else{
+      res.status(400).json('User Not Found');
+    }
+  })
+});
+
 
 module.exports = router;
