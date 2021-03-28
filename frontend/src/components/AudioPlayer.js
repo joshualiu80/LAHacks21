efb4bf20fac0e4abdeb60d1a1e1763c3b4167322
nguyenethan01 <nguyenethan01@gmail.com>
2021-03-28 02:48:03 -0700

    snippet formatting

--- frontend/src/components/AudioPlayer.js
+++ frontend/src/components/AudioPlayer.js
@@ -1,20 +1,15 @@
 import React from 'react';
 import './AudioPlayer.css';
 
-class AudioPlayer extends React.Component {
-	constructor(props) {
-		super(props);
-	}
-
-	render() {
-		return (
-			<div className="audio-player">
-				<audio controls controlsList="nodownload" src={`http://localhost:3000/snippets/files/${this.props.audio}`}>
-					Audio element not supported
-				</audio>
-			</div>
-		);
-	}
+const AudioPlayer = (props) => {
+	const { audio } = props;
+	return (
+		<div className="audio-player">
+			<audio controls controlsList="nodownload" src={`http://localhost:3000/snippets/files/${audio}`}>
+				Audio element not supported
+			</audio>
+		</div>
+	);
 }
 
 export default AudioPlayer;
--- frontend/src/pages/feedPage.js
+++ frontend/src/pages/feedPage.js
@@ -1,22 +1,35 @@
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import { withCookies, Cookies } from 'react-cookie';
+import AudioPlayer from '../components/AudioPlayer';
 import './feedPage.css';
+const exampleIMG = "https://images.ctfassets.net/hrltx12pl8hq/6YSoTmOYPk2VtQ7JSkPuzS/8250a3d54c1a714aa5e57f6a2826509e/shutterstock_1554086789.jpg?fit=fill&w=480&h=270";
 
 const FeedPage = (props) => {
   const { cookies } = props;
   const [tags, setTags] = useState([]);
-  const [currTab, setCurrTab] = useState(0);
-  const [snippetMap, setSnippetMap] = useState(null);
   const [newSnippets, setNewSnippets] = useState([]);
+  const [snippetMap, setSnippetMap] = useState(null);
+  const [currTab, setCurrTab] = useState(0);
 
   const renderTags = () => {
     return (
       <div className="tag-row">
-        <p className="tag-button" onClick={() => { setCurrTab(0) }}>For You</p>
-        {tags.map((tag, index) => {
-          return <p className="tag-button" onClick={() => { setCurrTab(index+1) }} >{tag.name}</p>
-        })}
+        <p
+          className={`tag-button ${currTab === 0 ? 'selected' : ''}`}
+          onClick={() => { setCurrTab(0) }}
+        >
+          For You
+        </p>
+        {
+          tags.map((tag, index) => {
+            return <p
+              className={`tag-button ${currTab-1 === index ? 'selected' : ''}`}
+              onClick={() => { setCurrTab(index+1) }}
+              style={{'backgroundColor': {}}}
+            >{tag.name}</p>
+          })
+        }
       </div>
     );
   };
@@ -26,30 +39,31 @@ const FeedPage = (props) => {
     const feedSnippets = currTab === 0 ? newSnippets : snippetMap.get(tags[currTab-1]._id);
     
     // if the user wants to see their own personal feed
-    if (currTab === 0) {
-      return (
-        <div>
-        <h1 className="feed-title">Your Snippets</h1>
-        <div className="snippets-list">
-          {
-            feedSnippets.length === 0
-            ? (<p className="no-snippets-message">No snippets to show :(</p>) :
-            (<div>posts</div>)
-          }
-        </div>
-      </div>
-      )
-    }
-
-    // render the snippets classified under any other tags
+    
     return (
       <div>
         <h1 className="feed-title">{currTab === 0 ? 'Your Snippets' : tags[currTab-1].name}</h1>
         <div className="snippets-list">
           {
-            feedSnippets.length === 0
+            feedSnippets === undefined || feedSnippets.length === 0
             ? (<p className="no-snippets-message">No snippets to show :(</p>) :
-            (<div>posts</div>)
+            (<div>
+              {
+                feedSnippets.map((snippet) => {
+                  return (
+                    <div className="snippet-card">
+                      <div className="snippet-card-left-div">
+                        <div className="bubble">
+                          <img className="snippet-image" src={exampleIMG}/>
+                        </div>
+                        <p className="snippet-name">{snippet.title}</p>
+                      </div>
+                      <AudioPlayer audio={snippet.fileName} />
+                    </div>
+                  );
+                })
+              }
+            </div>)
           }
         </div>
       </div>
@@ -92,13 +106,20 @@ const FeedPage = (props) => {
   }, []); 
 
   return (
-    <div>
+    <div className="outermost">
       <div className="navbar-placeholder"></div>
-      <div className="tags-section">
-        {renderTags()}
-      </div>
-      <div className="feed-section">
-        {renderFeed()}
+      <div className="feed-page-container">
+        <div className="feed-page-left-side">
+          <div className="tags-section">
+            {renderTags()}
+          </div>
+          <div className="feed-section">
+            {renderFeed()}
+          </div>
+        </div>
+        <div className="feed-page-right-sidebar">
+          <p className="create-recording-button">Create Recording</p>
+        </div>
       </div>
     </div>
   );
